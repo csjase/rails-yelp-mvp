76535e20af2b8d31d7227a94205d017f93326552
Jason Sim <jschinsin5@hotmail.com>
2020-05-27 21:45:31 +1000

    finally

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  validates :content, :rating, presence: true
+  validates :rating, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
+  belongs_to :restaurant
+  validates :content, presence: true
+end
